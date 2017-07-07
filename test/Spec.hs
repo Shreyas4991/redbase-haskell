d57637bb86c9ff18e65363aa7b1acf2bb44cf8a2
Shreyas94 <shreyasss94@gmail.com>
2017-07-07 02:31:14 +0100

    Made some changes to Spec.hs file (containing tests)

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,11 +4,12 @@ import Utilities.Errors
 import PageFiles.File as File
 import PageFiles.Page as Page
 import System.IO
-import Data.ByteString as BS
+
 import Data.Binary
 import GHC.Generics(Generic)
-import Control.Monad
+import Control.Monad (void)
 
+-- Test on management of records
 data Record  = Person {
     name::String,
     age::Int
@@ -29,16 +30,27 @@ writeRecord :: Handle -> Page Record -> IO ()
 writeRecord = writePage
 
 
-main :: IO ()
-main = void $ open "test.dat"
+testRecord :: IO ()
+testRecord = open "test.dat"
         >>= \handle ->
-                writePage handle p
+                writePageAt handle 0 p
                     >> writePage handle q
                     >> readRecordAt handle 0 (byteLength p)
                         >>= System.IO.putStrLn.show
                         >> readRecord handle (byteLength p)
                             >>= System.IO.putStrLn.show
-
+                            >> writePageAt handle 0 r
+                            >> readRecordAt handle 0 (byteLength r)
+                                >>= System.IO.putStrLn.show
+                                >> readRecord handle (byteLength p)
+                                    >>= System.IO.putStrLn.show
         where
-            p = Page {pageNumber=1,contents = Person {name="Shreyas",age = 22}}
-            q = Page {pageNumber=2,contents = Person {name="Thejas",age=18}}
+            p = Page {pageNumber=1,contents = Person {name="Finch",age = 22}}
+            q = Page {pageNumber=2,contents = Person {name="Reese",age = 18}}
+            r = Page {pageNumber=3,contents = Person {name="Fusco",age = 20}}
+
+
+-- The main function executing all tests
+
+main :: IO ()
+main = void testRecord
